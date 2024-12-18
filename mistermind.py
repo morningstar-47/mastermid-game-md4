8854630899d22ef8052ca355b158518828be2e87
Claude Emmanuel Mopeno-Bia <mopenobiaclaude@gmail.com>
2024-12-18 13:41:55 +0100

    Ajout de la fonction maitresse terminal

--- mistermind.py
+++ mistermind.py
@@ -42,3 +42,36 @@ def checking_guess(guess, combinaison):
     correct_color -= correct_position
 
     return correct_position, correct_color
+
+
+def mastermind_terminal():
+    code = generate_code()
+    attempts = 0
+
+    print("Bienvenue à Mastermind MD4 Hetic!")
+    print(f"Essayez de deviner la combinaison de {
+          COMBINAISON_LENGTH} couleurs.")
+    print(f"Couleurs possibles : {', '.join(COLORS)}.")
+    print(f"Vous avez {ATTEMPTS_MAX} essais au total.")
+
+    while attempts < ATTEMPTS_MAX:
+        print(f"Essai {attempts + 1} sur {ATTEMPTS_MAX}.")
+        guess = get_player_guess()
+
+        correct_position, correct_color = checking_guess(guess, code)
+        print(f"Couleurs bien placées : {correct_position}")
+        print(f"Couleurs correctes mais mal placées : {correct_color}")
+
+        if correct_position == COMBINAISON_LENGTH:
+            print("Félicitations! Vous avez deviné la combinaison!")
+            return
+
+        attempts += 1
+        print(f"Il vous reste {ATTEMPTS_MAX - attempts} essai(s).")
+
+    print(f"Désolé, vous avez utilisé tous vos essais.")
+    print(f"La combinaison correcte était : {' '.join(code)}")
+
+
+if __name__ == "__main__":
+    mastermind_terminal()
